c2f58922eaa22278eef6c13d4def36742b997b18
sheepfling <sheepfling@users.noreply.github.com>
2026-09-20 00:46:59 -0500

    "portable-standard-timestamped-interaction-mixed-advances"

--- packages/hla-rti-cpp-tck/src/portable_tck.cpp
+++ packages/hla-rti-cpp-tck/src/portable_tck.cpp
@@ -399,6 +399,10 @@ constexpr char availableTimeAdvancesInclusiveGaltScenario[] =
     "cpp-tck.available-time-advances-inclusive-galt";
 constexpr char availableTimeAdvancesInclusiveGaltContractId[] =
     "cpp-tck.available-time-advances-inclusive-galt-contract";
+constexpr char timestampedInteractionMixedAdvancesScenario[] =
+    "cpp-tck.timestamped-interaction-mixed-advances";
+constexpr char timestampedInteractionMixedAdvancesContractId[] =
+    "cpp-tck.timestamped-interaction-mixed-advances-contract";
 constexpr char modifyLookaheadScenario[] = "cpp-tck.modify-lookahead";
 constexpr char modifyLookaheadContractId[] =
     "cpp-tck.modify-lookahead-contract";
@@ -1356,6 +1360,18 @@ void scenarioAvailableTimeAdvancesInclusiveGaltContractPortable(
     Options const& options,
     rti::CallbackModel model) {
   scenarioAvailableTimeAdvancesInclusiveGaltContract(options, model);
+}
+
+void scenarioTimestampedInteractionMixedAdvancesPortable(
+    Options const& options,
+    rti::CallbackModel model) {
+  scenarioTimestampedInteractionMixedAdvances(options, model);
+}
+
+void scenarioTimestampedInteractionMixedAdvancesContractPortable(
+    Options const& options,
+    rti::CallbackModel model) {
+  scenarioTimestampedInteractionMixedAdvancesContract(options, model);
 }
 
 void scenarioSynchronizationPointsPortable(
@@ -9165,6 +9181,16 @@ int runAvailableTimeAdvancesInclusiveGaltScenarios(int argc, char** argv) {
       scenarioAvailableTimeAdvancesInclusiveGaltContractPortable);
 }
 
+int runTimestampedInteractionMixedAdvancesScenarios(int argc, char** argv) {
+  return runPortableScenarioPair(
+      argc,
+      argv,
+      timestampedInteractionMixedAdvancesScenario,
+      timestampedInteractionMixedAdvancesContractId,
+      scenarioTimestampedInteractionMixedAdvancesPortable,
+      scenarioTimestampedInteractionMixedAdvancesContractPortable);
+}
+
 int runUnconditionalAttributeOwnershipDivestitureScenarios(
     int argc,
     char** argv) {
@@ -10225,6 +10251,20 @@ bool hasAvailableTimeAdvancesInclusiveGaltScenario(int argc, char** argv) {
     auto const scenario = std::string(argv[index + 1]);
     if (scenario == availableTimeAdvancesInclusiveGaltScenario ||
         scenario == availableTimeAdvancesInclusiveGaltContractId) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool hasTimestampedInteractionMixedAdvancesScenario(int argc, char** argv) {
+  for (int index = 1; index + 1 < argc; ++index) {
+    if (std::string(argv[index]) != "--scenario") {
+      continue;
+    }
+    auto const scenario = std::string(argv[index + 1]);
+    if (scenario == timestampedInteractionMixedAdvancesScenario ||
+        scenario == timestampedInteractionMixedAdvancesContractId) {
       return true;
     }
   }
@@ -11608,6 +11648,9 @@ int main(int argc, char** argv) {
     }
     if (hasAvailableTimeAdvancesInclusiveGaltScenario(argc, argv)) {
       return runAvailableTimeAdvancesInclusiveGaltScenarios(argc, argv);
+    }
+    if (hasTimestampedInteractionMixedAdvancesScenario(argc, argv)) {
+      return runTimestampedInteractionMixedAdvancesScenarios(argc, argv);
     }
     if (hasModifyLookaheadScenario(argc, argv)) {
       return runModifyLookaheadScenarios(argc, argv);
